a474cab7c1a9345251c54c8b8c0412d8a85e59b6
Eddy Ramos <eddyinf605@gmail.com>
2023-01-26 16:17:21 -0400

    conexion bd

---
+++ clases/index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class conectar
+{
+    private $servidor = "localhost";
+    private $usuario = "root";
+    private $password = "";
+    private $bd = "ventas";
+
+    public function conexion()
+    {
+        $conexion = mysqli_connect(
+            $this->servidor,
+            $this->usuario,
+            $this->password,
+            $this->bd
+        );
+        return $conexion;
+    }
+}
+$obj = new conectar();
+if ($obj -> conexion()) {
+    echo 'conectado';
+}
